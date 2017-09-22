61caab273b871dd12595d6cbe50f70b355f0e8b9
LapAdrian <luis.gvargas@alumnos.udg.mx>
2017-09-22 17:53:31 -0500

    Se comenzo a leer el archivo
    El archivo ya es leido y se valida que la entrada no tenga partes
    "mochas", e guardan los datos en una matriz para saber como se encuentra
    el mapa.

--- src/sample/Mapa.java
+++ src/sample/Mapa.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by blalb on 21/09/2017.
@@ -13,6 +13,8 @@ public class Mapa {
 
     private GraphicsContext gc;
     private int numFilas, numColumnas;
+    //Por convencion [Filas] [Columnas]
+    private int[][] matrizmapa;
 
     public Mapa(GraphicsContext gc){
 
@@ -21,22 +23,56 @@ public class Mapa {
         this.numColumnas = 0;
     }
 
-    public void leerArchivo(String fileName){
+    public boolean leerArchivo(String fileName){
         Path path = Paths.get(fileName);
+        int contFilas = 0;
+        ArrayList<ArrayList<Integer>> matriz = new ArrayList<ArrayList<Integer>>();
         try(Scanner scanner = new Scanner(path)){
             while (scanner.hasNextLine()){
-                leerFila(scanner.nextLine());
+                ArrayList<Integer> fila = new ArrayList<Integer>();
+                //Si no coinciden las columnas
+                if(!leerFila(scanner.nextLine(), fila)){
+                    //Termina de buscar, no funcionan las columnas, no hay necesidad de buscar mas
+                    return false;
+                }
+                matriz.add(fila);
+                contFilas++;
             }
+            numFilas = contFilas;
         } catch (IOException e){
             System.out.println("No se pudo leer el archivo");
         }
+        matrizmapa = llenarMatrizMapa(matriz);
+        return true;
     }
 
-    private void leerFila(String line){
+    private boolean leerFila(String line, ArrayList<Integer> fila){
         Scanner scanner = new Scanner(line).useDelimiter("\\s*,\\s*");
+        int contColumnas = 0;
+        boolean correcto = true;
         while (scanner.hasNextInt()){
             //System.out.print(scanner.nextInt() + " ");
-
+            fila.add(scanner.nextInt());
+            contColumnas++;
         }
+        if(numColumnas!=0){
+            if(contColumnas!=numColumnas){
+                correcto = false;
+            }
+        } else {
+            numColumnas = contColumnas;
+        }
+        return correcto;
+    }
+
+    private int[][] llenarMatrizMapa(ArrayList<ArrayList<Integer>> matrizEnLista){
+        int[][] matriz = new int[numFilas][numColumnas];
+        int i, j;
+        for (i=0; i<numFilas; i++){
+            for (j=0; j<numColumnas; j++){
+                matriz[i][j] = (matrizEnLista.get(i)).get(j);
+            }
+        }
+        return matriz;
     }
 }
